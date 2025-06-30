a4c44e6202d5b6ca34d80a58892e2e4fffdf1010
Siddharth Agrawal <siddharth2004agrawal@gmail.com>
2025-06-30 20:33:46 +0530

    Update loc App.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,8 @@ function App() {
        <a href="https://www.instagram.com/chiragtiles?igsh=MXBpb3dtbWRic2Zrbw==" className='insta'>Instagram</a>
        <a href="https://www.facebook.com/share/1EwhCAVK5i/?mibextid=wwXIfr" className='FB'>Facebook</a>
        <a href="https://wa.me/message/63XLCE7BZHDHK1" className='WB'>WhatsApp</a>
-        <a href="https://www.bing.com/maps?&ty=18&q=chirag%20tiles%20and%20sanitary%20store%20khair%20aligarh&ss=ypid.YN4070x4056037265260737082&mb=27.
-        868074~78.075671~27.859974~78.088331&description=19%2F426%2C%20Pala%20Holi%20Chowk%2C%20NA%2C%20Pala%20Road%2C%20Aligarh%2C%20Uttar%20Pradesh%20202001%C2%B7Plumbing&cardbg=%23F98745&dt=1751268600000&tt=Chirag%20Sanitary%20House&tsts0=%2526ty%253D18%2526q%253Dchirag%252520tiles%252520and
-        %252520sanitary%252520store%252520khair%252520aligarh%2526ss%253Dypid.YN4070x4056037265260737082%2526mb%253D27.868074~78.075671~27.859974~78.088331%2526description%253D19%25252F426%25252C%252520Pala%252520Holi%252520Chowk%25252C%252520NA%25252C%252520Pala%252520Road%25252C%252520Aligarh%
-        25252C%252520Uttar%252520Pradesh%252520202001%2525C2%2525B7Plumbing%2526cardbg%253D%252523F98745%2526dt%253D1751268600000&tstt0=Chirag%20Sanitary%20House&cp=27.864286~78.079849&lvl=17.336079&pi=0&ftst=0&ftics=False&v=2&sV=2&form=S00027" className='Loc'>Location</a>
-       </div>
+        <a href="https://maps.app.goo.gl/9NgPGESS5nXiZSe79?g_st=ipc" className='Loc'>Location<a>
+    </div>
       </header>
     </div>
   );
